1478ea48b8bc54dbc13ba9dc9154e470ff794a14
vravuri <vravuri@andrew.cmu.edu>
2013-04-17 14:08:29 -0400

    Scchema changes.

--- src/main/java/edu/cmu/cs/lti/oaqa/graphqa/db/schema/SchemaConstants.java
+++ src/main/java/edu/cmu/cs/lti/oaqa/graphqa/db/schema/SchemaConstants.java
@@ -8,12 +8,12 @@ package edu.cmu.cs.lti.oaqa.graphqa.db.schema;
  */
 public class SchemaConstants {
 
-	// Synonyms for professor entity used by crawler
+	// Synonyms for professor entity used by crawler module
 	public static String PROFESSOR_ENTITY_SYNONYM_PROFESSOR = "professor";
 	public static String PROFESSOR_ENTITY_SYNONYM_TEACHER = "teacher";
 	public static String PROFESSOR_ENTITY_SYNONYM_FACULTY = "faculty";
 
-	// Synonyms for course entity used by crawler
+	// Synonyms for course entity used by crawler module
 	public static String COURSE_ENTITY_SYNONYM_COURSE = "course";
 
 }
